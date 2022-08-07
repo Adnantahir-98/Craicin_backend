35af2091d9c527f52794ebcdf8448f2d9eebb027
hamzahashmi3 <technocannons3@gmail.com>
2022-08-07 15:24:24 +0500

    undefined feat(tour API's): tour API's

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const Booktour = require('./routes/api/booktour.route')
 const app = express()
-
+const cors = require('cors')
 // Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false}))
+app.use(cors())
 
 // Routes
 app.get('/', (req, res) => res.send('API Running, Home Page'))
-app.use('/booktour', require('./routes/api/booktour'))
+app.use('/api', Booktour)
 
 
 const PORT = process.env.PORT || 5000
